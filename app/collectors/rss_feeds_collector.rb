91d917dedb27cdee65239a6dbc155c519222938e
carbon13 <yamada@hamacbook.local>
2015-11-11 09:35:33 +0900

    modify xpath description & add exclude_tags method.

--- app/collectors/rss_feeds_collector.rb
+++ app/collectors/rss_feeds_collector.rb
@@ -18,21 +18,21 @@ class RssFeedsCollector
     results.each do |rss_source_link, result|
       result.xpath('//channel').each do |node|
         rss_source = RssSource.find_by(link: rss_source_link)
-        rss_source.title = node.xpath('./title').text
-        # FIXME: rss_source.atom_link = node.xpath('./atom:link').attribute('href').value
-        rss_source.description = node.xpath('./description').text
-        rss_source.last_build_date = Time.rfc2822(node.xpath('./lastBuildDate').text)
-        rss_source.language = node.xpath('./language').text
-        rss_source.generator = node.xpath('./generatorTime').text
+        rss_source.title = node.xpath('./title').first.try(:text)
+        # FIXME: Undefined namespace prefix 'atom:'; rss_source.atom_link = node.xpath('./atom:link').attribute('href').first.try(:value)
+        rss_source.description = node.xpath('./description').first.try(:text)
+        rss_source.last_build_date = Time.rfc2822(node.xpath('./lastBuildDate').first.try(:text))
+        rss_source.language = node.xpath('./language').first.try(:text)
+        rss_source.generator = node.xpath('./generatorTime').first.try(:text)
         rss_source.save
       end
       result.xpath('//item').each do |node|
-        unless RssFeed.find_by(title: node.xpath('.//title').text)
+        unless RssFeed.find_by(title: node.xpath('.//title').try(:text))
           rss_feed = RssFeed.new
-          rss_feed.title = node.xpath('.//title').text
-          rss_feed.description = node.xpath('.//description').text
-          rss_feed.link = node.xpath('.//link').text
-          rss_feed.pubdate = Time.rfc2822(node.xpath('.//pubDate').text)
+          rss_feed.title = node.xpath('.//title').try(:text)
+          rss_feed.description = node.xpath('.//description').try(:text)
+          rss_feed.link = node.xpath('.//link').try(:text)
+          rss_feed.pubdate = Time.rfc2822(node.xpath('.//pubDate').try(:text))
           rss_feed.save 
         end
       end
@@ -47,18 +47,22 @@ class RssFeedsCollector
       parsed_body = Nokogiri::HTML.parse(open(target_rss_feed.link, redirect: true))
       parsed_results.store(target_rss_feed.id, parsed_body)
     end
-    parsed_results
+    return parsed_results
   end
 
   def store_bodies(results)
     results.each do |rss_feed_id, result|
-      exclusive_tag_names = ['script', 'head']
-      exclusive_tag_names.each do |tag_name|
-        result.root.xpath("//#{tag_name}").each { |node| node.remove }
-      end
       trimed_body = result.xpath('//body').inner_text.gsub(/^[\s　]+|[\s　]+$/, "").gsub(/(\r\n|\r|\n)/, ' ')
       rss_feed = RssBody.new(rss_feed_id: rss_feed_id, body: trimed_body)
       rss_feed.save
     end
+  end
+
+  def exclude_tags(result, additional_exclusive_tag_names=[])
+    exclusive_tag_names = ['script', 'head'].concat(additional_exclusive_tag_names)
+    exclusive_tag_names.each do |tag_name|
+      result.root.xpath("//#{tag_name}").each { |node| node.remove }
+    end
+    return result
   end
 end
