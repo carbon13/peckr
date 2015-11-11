b7ae20dc8e408dbed28558138fd17edd845696c2
carbon13 <yamada@hamacbook.local>
2015-11-11 09:40:37 +0900

    add an association.

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 class Pair < ActiveRecord::Base
   has_many :xchanges, primary_key: :pair_id, foreign_key: :pair_id
+  has_many :floaters, primary_key: :pair_id, foreign_key: :pair_id
 
   scope :selected, -> { where{ selected == 1} }
   scope :pair_id_in, ->(pair_ids) { where{ pair_id >> pair_ids } }
