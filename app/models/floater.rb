690b023547df79478378401d6de2589b81654211
carbon13 <yamada@hamacbook.local>
2015-11-11 09:40:17 +0900

    change where clause to squeal description.
    & add a an association.

--- app/models/floater.rb
+++ app/models/floater.rb
@@ -1,12 +1,13 @@
 # encoding: utf-8
 class Floater < ActiveRecord::Base
   belongs_to :floater_type
+  belongs_to :pair
 
-  scope :active, -> { where(active: 1).where('durability > ?', 0) }
-  scope :inactive, -> { where(active: 0) }
-  scope :thresholds, -> { where(floater_type_id: 1) }
-  scope :timers, -> { where(floater_type_id: 2)}
-  scope :accelations, -> { where(floater_type_id: 3) }
+  scope :active, -> { where{active = 1}.where{durability > 0}.where{(start_at <= Time.now) & (end_at >= Time.now)} }
+  scope :inactive, -> { where{ active = 0 } }
+  scope :thresholds, -> { where{ floater_type_id = 1} }
+  scope :timers, -> { where{ floater_type_id = 2 } }
+  scope :accelations, -> { where{ floater_type_id = 3 } }
 
   def hit?
     raise
