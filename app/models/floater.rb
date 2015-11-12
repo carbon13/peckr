2a55500e4b530a1ff7f88393cbedfe564d52ecdd
carbon13 <yamada@hamacbook.local>
2015-11-12 13:17:14 +0900

    change where clause to squeal description.

--- app/models/floater.rb
+++ app/models/floater.rb
@@ -3,13 +3,14 @@ class Floater < ActiveRecord::Base
   belongs_to :floater_type
   belongs_to :pair
 
-  scope :active, -> { where{active = 1}.where{durability > 0}.where{(start_at <= Time.now) & (end_at >= Time.now)} }
-  scope :inactive, -> { where{ active = 0 } }
-  scope :thresholds, -> { where{ floater_type_id = 1} }
-  scope :timers, -> { where{ floater_type_id = 2 } }
-  scope :accelations, -> { where{ floater_type_id = 3 } }
+  scope :active, -> { where{ active == 1 }.where{ durability > 0 }.where{ (start_at <= Time.now) & (end_at >= Time.now) } }
+  scope :inactive, -> { where{ active == 0 } }
+  scope :thresholds, -> { where{ floater_type_id == 1 } }
+  scope :velocity, -> { where{ floater_type_id == 2 } }
+  scope :timers, -> { where{ floater_type_id == 3 } }
+  scope :state, -> { where{ floater_type_id == 4 } }
 
-  def hit?
+  def struck?(current_xchange)
     raise
   end
 end
